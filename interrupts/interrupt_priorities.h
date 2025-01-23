911934e8d199982bf0aa16c068a5e5d7c966283a
peredelkin <ep8kha@gmail.com>
2025-01-23 12:51:37 +0500

    изменены приоритеты прерываний

--- interrupts/interrupt_priorities.h
+++ interrupts/interrupt_priorities.h
@@ -10,13 +10,14 @@
 
 #define TIM2_IRQ_PRIO 1		//SYS_CNT
 
-#define SPI2_IRQ_PRIO 4		//SPI2
-#define SPI4_IRQ_PRIO 4		//SPI4
+#define SPI2_IRQ_PRIO 2		//SPI2
+#define SPI4_IRQ_PRIO 2		//SPI4
+
+#define CAN1_IRQ_PRIO 3		//Socket 3
+#define UART6_IRQ_PRIO 3	//Socket 3
+#define UART7_IRQ_PRIO 3	//RS485 Panel
 
 #define UART3_IRQ_PRIO 5	//UART3 + DMA Stream
-#define UART6_IRQ_PRIO 5	//UART6
-#define UART7_IRQ_PRIO 5	//UART7
-#define CAN1_IRQ_PRIO 5		//CAN1
 
 #define TIM4_IRQ_PRIO 7		//MS_TIM
 #define TIM3_IRQ_PRIO 8		//SYS_TIM
