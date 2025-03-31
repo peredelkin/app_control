714f3f230a67328ec53beab94e9704944437bb20
peredelkin <ep8kha@gmail.com>
2025-03-31 20:10:22 +0500

    temp calc

--- temp/ntc/ntc_temp.c
+++ temp/ntc/ntc_temp.c
@@ -18,7 +18,9 @@ METHOD_DEINIT_IMPL(M_ntc_temp, ntc_temp)
 }
 
 int bsearch_ohm_comparator(const void* ptr1, const void* ptr2) {
-return 0;
+	if(*((float*)ptr1) > ((ntc_point_t*)ptr2)->kohm) return 1;
+	if(*((float*)ptr1) < ((ntc_point_t*)ptr2)->kohm) return -1;
+	return 0;
 }
 
 uint32_t ntc_counter;
@@ -28,9 +30,9 @@ METHOD_CALC_IMPL(M_ntc_temp, ntc_temp) {
 	iq15_t vin = (iq15_t)(msdi.out_analog[ntc_counter] * IQN_BASE(5, int32_t));
 	iq15_t R_ref_voltage = vref - vin;
 
-	ntc_temp->out_ohm[ntc_counter] = (ntc_temp->m_R_ref * msdi.out_analog[ntc_counter])/R_ref_voltage;
+	ntc_temp->out_ohm[ntc_counter] = (ntc_temp->m_R_ref * vin)/R_ref_voltage;
 
-	int32_t R_in = ntc_temp->out_ohm[ntc_counter];
+	iq15_t R_in = ntc_temp->out_ohm[ntc_counter];
 
 	if(R_in > ntc_temp->m_R_in_max) {
 		R_in = ntc_temp->m_R_in_max;
@@ -40,8 +42,10 @@ METHOD_CALC_IMPL(M_ntc_temp, ntc_temp) {
 		R_in = ntc_temp->m_R_in_min;
 	}
 
+	float float_R_in = (float)(R_in)/(float)(IQ15_BASE);
+
 	ntc_point_t* ntc_ptr = bsearch(
-			&R_in,
+			&float_R_in,
 			ntc_4901_table,
 			NTC_4901_TABLE_SIZE,
 			sizeof(ntc_point_t),
