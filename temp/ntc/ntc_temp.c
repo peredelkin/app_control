4f7f7eba37632720f6ebb92321c69d11b86d53b6
peredelkin <ep8kha@gmail.com>
2025-04-07 10:51:11 +0500

    линейная интерполяция температуры

--- temp/ntc/ntc_temp.c
+++ temp/ntc/ntc_temp.c
@@ -8,7 +8,7 @@
 METHOD_INIT_IMPL(M_ntc_temp, ntc_temp)
 {
 	ntc_temp->m_R_in_max = (iq15_t)(ntc_4901_table[0].kohm * IQ15_BASE);
-	ntc_temp->m_R_in_min = (iq15_t)(ntc_4901_table[NTC_4901_TABLE_SIZE - 1].kohm * IQ15_BASE);
+	ntc_temp->m_R_in_min = (iq15_t)(ntc_4901_table[NTC_4901_TABLE_SIZE - 2].kohm * IQ15_BASE);
 	ntc_temp->m_R_ref = (iq15_t)(3.3f * IQ15_BASE);
 }
 
@@ -18,17 +18,23 @@ METHOD_DEINIT_IMPL(M_ntc_temp, ntc_temp)
 }
 
 int bsearch_ohm_comparator(const void* ptr1, const void* ptr2) {
-	if(*((float*)ptr1) > ((ntc_point_t*)ptr2)->kohm) return 1;
-	if(*((float*)ptr1) < ((ntc_point_t*)ptr2)->kohm) return -1;
+	float R_in = *((float*)ptr1);
+	//a > b
+	float R_a = ((ntc_point_t*)ptr2)[0].kohm;
+	float R_b = ((ntc_point_t*)ptr2)[1].kohm;
+	if(R_in > R_a) return -1;
+	if(R_in < R_b) return 1;
 	return 0;
 }
 
 uint32_t ntc_counter;
 
 METHOD_CALC_IMPL(M_ntc_temp, ntc_temp) {
-	iq15_t vref = (iq15_t)(msdi.out_analog[6] * IQN_BASE(5, int32_t));
-	iq15_t vin = (iq15_t)(msdi.out_analog[ntc_counter] * IQN_BASE(5, int32_t));
+	iq15_t vref = (iq15_t)(msdi.out_analog[6] * IQN_BASE(4, int32_t));
+	iq15_t vin = (iq15_t)(msdi.out_analog[ntc_counter] * IQN_BASE(4, int32_t));
 	iq15_t R_ref_voltage = vref - vin;
+
+	if(R_ref_voltage == 0) R_ref_voltage = 1;
 
 	ntc_temp->out_ohm[ntc_counter] = (ntc_temp->m_R_ref * vin)/R_ref_voltage;
 
@@ -51,8 +57,18 @@ METHOD_CALC_IMPL(M_ntc_temp, ntc_temp) {
 			sizeof(ntc_point_t),
 			&bsearch_ohm_comparator);
 
-	if(ntc_ptr != NULL) {
-		ntc_temp->out_temp[ntc_counter] = ntc_ptr->temp;
+	if (ntc_ptr != NULL) {
+		float R_a = ntc_ptr[0].kohm;
+		float Temp_a = ntc_ptr[0].temp;
+
+		float R_b = ntc_ptr[1].kohm;
+		float Temp_b = ntc_ptr[1].temp;
+
+		float Temp = Temp_a + ((Temp_b - Temp_a) * (R_a - float_R_in)) / (R_a - R_b);
+
+		ntc_temp->out_temp[ntc_counter] = (iq15_t) (Temp * IQ15_BASE);
+	} else {
+		ntc_temp->out_temp[ntc_counter] = IQ15_MAX;
 	}
 
 	if (ntc_counter >= (NTC_TEMP_COUNT - 1)) {
